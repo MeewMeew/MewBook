c3c9bc48adaf37549510f64db80a508253ff41d8
Nguyễn Mậu Minh <mauminh.nguyen@gmail.com>
2023-10-10 07:43:33 +0700

    chore: update css class

--- src/views/Friend/FriendLayout.vue
+++ src/views/Friend/FriendLayout.vue
@@ -4,7 +4,7 @@ import MainNavLayout from '@/layouts/MainNavLayout.vue'
 <template>
   <MainNavLayout select="friends">
     <div class="fixed w-full h-full bg-[#F1F2F5] top-0 flex flex-row pt-10">
-      <div class="hidden h-full bg-white w-1/4 shadow-xl mt-4 p-4 md:flex flex-col min-w-xs">
+      <div class="hidden h-full bg-white w-1/4 shadow-xl mt-4 p-4 md:flex flex-col min-w-[380px]">
         <slot name="sidebar"></slot>
       </div>
       <div class="w-full mt-4">
